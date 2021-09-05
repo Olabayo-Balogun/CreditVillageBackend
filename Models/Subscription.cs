884ee677fe930a43adf462787e68c465b19fee6f
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-09-05 20:17:35 +0100

    Subscription model class created

---
+++ Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditVillageBackend.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTimeOffset Duration { get; set; }
+    }
+}
